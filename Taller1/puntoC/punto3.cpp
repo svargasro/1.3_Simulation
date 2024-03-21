f2800c953ae40472ae8ba93e7174bee5e1803010
svargasro <svargasro@unal.edu.co>
2024-03-21 16:45:08 -0500

    Punto 3 Falta grafico bonito

--- Taller1/puntoC/punto3.cpp
+++ Taller1/puntoC/punto3.cpp
@@ -11,7 +11,7 @@ using std::sin;
 using std::cos;
 
 //Constantes del problema físico
-const int N=2;
+const int N=3;
 const double G=1.0;
 
 //Constantes del algoritmo de integración
@@ -90,11 +90,18 @@ void Colisionador::CalculeFuerzaEntre(Cuerpo & Planeta1,Cuerpo & Planeta2){
 int main(){
   double r=1000.0; //Distancia entre Júpiter y el Sol.
   double m0= 1047.0;  //Masa del sol.
-  double m1=1; //Masa de Júpiter.
+  double m1= 1; //Masa de Júpiter.
   double M=m0+m1, mu=m0*m1/M;
   double x0=-m1*r/M,x1=m0*r/M;
   double omega=sqrt(G*M/(r*r*r)); double T=2*M_PI/omega;
   double V0=omega*x0, V1=omega*x1;
+  //Variables tercer cuerpo.
+  double x2 = x1*cos(M_PI/3.0); //Posición inicial en x
+  double y2 = x1*sin(M_PI/3.0); //Posición inicial en y
+  double V2x = -omega*y2; //Velocidad inicial en x
+  double V2y = omega*x2; //Velocidad inicial en y
+  double m2= 0.005;
+
   int numOrbitas = 20;
   double t, ttotal=numOrbitas*T;
 
@@ -107,18 +114,20 @@ int main(){
   //---------------(x0,y0,z0,Vx0,   Vy0,Vz0,m0,R0)
   Planeta[0].Inicie(x0, 0, 0,  0, V0,  0,m0,1.0);
   Planeta[1].Inicie(x1, 0, 0,  0, V1,  0,m1,0.5);
+  Planeta[2].Inicie(x2, y2, 0,  V2x, V2y,  0,m2,0.5);
 
   cout<<"Inicial sol"<<x0<<endl;
   cout<<"Inicial Júpiter"<<x1<<endl;
-
+  cout<<"Inicial troyano_x"<<x2<<endl;
+  cout<<"Inicial troyano_y"<<y2<<endl;
 
 
 
 
   std::ofstream fout;
   fout.open("data.txt");
-//  fout.precision(15);
-//  fout.setf(std::ios::scientific);
+  fout.precision(15);
+  fout.setf(std::ios::scientific);
 
   //Se calcula el movimiento.
   for(t=0;t<ttotal;t+=dt){
@@ -128,6 +137,10 @@ int main(){
   double x_j = Planeta[1].Getx();
   double y_j = Planeta[1].Gety();
 
+  double x_t = Planeta[2].Getx();
+  double y_t = Planeta[2].Gety();
+
+
   double omegaT= omega*t;
   double x_sp = x_s*cos(omegaT) + y_s*sin(omegaT);
   double y_sp = -x_s*sin(omegaT) + y_s*cos(omegaT);
@@ -135,8 +148,11 @@ int main(){
   double x_jp = x_j*cos(omegaT) + y_j*sin(omegaT);
   double y_jp = -x_j*sin(omegaT) + y_j*cos(omegaT);
 
+  double x_tp = x_t*cos(omegaT) + y_t*sin(omegaT);
+  double y_tp = -x_t*sin(omegaT) + y_t*cos(omegaT);
+
   // fout<<x_sp<<" "<<y_sp<<" "<<x_jp<<" "<<y_jp<<endl;
-  fout<<x_sp<<" "<<y_sp<<" "<<x_jp<<" "<<y_jp<<" "<<t<<endl;
+  fout<<x_sp<<" "<<y_sp<<" "<<x_jp<<" "<<y_jp<<" "<<" "<<x_tp<<" "<<y_tp<<" "<<t<<endl;
   //   fout<<Planeta[0].Getx()<<" "<<Planeta[0].Gety()<<" "<<Planeta[1].Getx()<<" "<<Planeta[1].Gety()<<endl;
 
   for(i=0;i<N;i++) Planeta[i].Mueva_r(dt,xi);
