af683e7a29501dd31a4ed3aa7c32e0dd633e174d
svargasro <svargasro@unal.edu.co>
2024-03-23 22:14:58 -0500

    puntoD terminado

--- Taller1/puntoD/plot.py
+++ Taller1/puntoD/plot.py
@@ -6,45 +6,17 @@ x_s, y_s, x_j, y_j, x_t, y_t, t = np.genfromtxt('data.txt', unpack=True, usecols
 
 plt.style.use('seaborn-v0_8')
 
-fig, axes = plt.subplots(3,1,figsize=(6, 6))
+fig, axes = plt.subplots(figsize=(6, 6))
 
 
-
-
-#axes.plot(x_s, y_s, '.', color='yellow', label=r'$(x,y)_{S}$')
-#axes.plot(x_j, y_j, '.', color='black', label=r'$(x,y)_{J}$')
-
-
-
-
-#t = np.linspace(0,len(x_s) , len(x_s))
-# axes[0].plot(t, x_s, '.', color='yellow', label=r'$(x)_{S}$')
-# axes[1].plot(t, y_s, '.', color='yellow', label=r'$(y)_{S}$')
-# axes[2].plot(t, x_j, '.', color='black', label=r'$(x)_{J}$')
-# axes[3].plot(t, y_j, '.', color='black', label=r'$(y)_{J}$')
-# axes[4].plot(t, x_t, '.', color='blue', label=r'$(x)_{T}$')
-# axes[5].plot(t, y_t, '.', color='blue', label=r'$(y)_{T}$')
-
-
-axes[0].plot(x_s, y_s, '.', color='yellow', label=r'$(x)_{S}$')
-axes[1].plot(x_j, y_j, '.', color='black', label=r'$(y)_{J}$')
-axes[2].plot(x_t, y_t, '.', color='blue', label=r'$(x)_{T}$')
-
-
-
-for i in range(0,3
-               ):
-    axes[i].legend()
-
-
-
-
-# Se ajustan demás detalles del gráfico.
-
-# axes.set_xlabel('x', fontsize=12)
-# axes.set_ylabel('y', fontsize=12)
-# axes.legend(loc='upper left')
-# axes.grid(True, linestyle='--')
-# axes.set_title("x vs y", fontsize=14)
+axes.plot(x_t, y_t, ".", color="cyan", label=r"$(x',y')_{T}$")
+axes.set_xlabel("x'", fontsize=12)
+axes.set_ylabel("y'", fontsize=12)
+axes.legend(loc='upper right')
+axes.grid(True, linestyle='--')
+axes.set_title("y' vs. x'. Perturbado en 5 partes por mil.", fontsize=14)
+axes.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 plt.tight_layout()
+
+
 fig.savefig('plot.pdf')
