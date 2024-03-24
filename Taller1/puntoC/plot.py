67cfa8247487c60bb79130a339e526a0cc7f7beb
svargasro <svargasro@unal.edu.co>
2024-03-23 22:02:38 -0500

    Punto C terminado

--- Taller1/puntoC/plot.py
+++ Taller1/puntoC/plot.py
@@ -1,44 +1,75 @@
 import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 
 
 x_s, y_s, x_j, y_j, x_t, y_t, t = np.genfromtxt('data.txt', unpack=True, usecols=(0, 1, 2, 3, 4, 5, 6))
 
+
+
 plt.style.use('seaborn-v0_8')
 
-fig, axes = plt.subplots(6,1,figsize=(6, 6))
+with PdfPages('plot.pdf') as pdf:
+
+    fig, axes = plt.subplots(4,1,figsize=(7, 7))
+
+    axes[0].plot(t, x_s, '.', color='yellow', label=r"$x^{'}(t)_{S}$")
+    axes[1].plot(t, y_s, '.', color='yellow', label=r"$y^{'}(t)_{S}$")
+    axes[2].plot(t, x_j, '.', color='black', label=r"$x^{'}(t)_{J}$")
+    axes[3].plot(t, y_j, '.', color='black', label=r"$y^{'}(t)_{J}$")
+
+
+    # Se ajustan demás detalles del gráfico.
+    for i in [0,2]:
+        axes[i].set_xlabel('t', fontsize=12)
+        axes[i].set_ylabel('x\'(t)', fontsize=12)
+        axes[i].legend(loc='upper left')
+        axes[i].grid(True, linestyle='--')
+        axes[i].set_title("x' vs. t'", fontsize=14)
+        axes[i].ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+
+    for i in [1,3]:
+        axes[i].set_xlabel('t', fontsize=12)
+        axes[i].set_ylabel('y\'(t)', fontsize=12)
+        axes[i].legend()
+        axes[i].grid(True, linestyle='--')
+        axes[i].set_title("y' vs. t", fontsize=14)
+        axes[i].ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+    plt.tight_layout()
+    pdf.savefig()  # saves the current figure into a pdf page
+    plt.close()
+
+
+    fig, axes = plt.subplots(3,1,figsize=(7, 7))
+
+    axes[0].plot(t, x_t, '.', color='cyan', label=r"$x^{'}(t)_{T}$")
+    axes[1].plot(t, y_t, '.', color='cyan', label=r"$y^{'}(t)_{T}$")
+
+
+    axes[0].set_xlabel('t', fontsize=12)
+    axes[0].set_ylabel('x\'(t)', fontsize=12)
+    axes[0].legend(loc='upper right')
+    axes[0].grid(True, linestyle='--')
+    axes[0].set_title("x' vs. t'", fontsize=14)
+    axes[0].ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+
+
+    axes[1].set_xlabel('t', fontsize=12)
+    axes[1].set_ylabel('y\'(t)', fontsize=12)
+    axes[1].legend()
+    axes[1].grid(True, linestyle='--')
+    axes[1].set_title("y' vs. t", fontsize=14)
+    axes[1].ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 
 
 
-
-#axes.plot(x_s, y_s, '.', color='yellow', label=r'$(x,y)_{S}$')
-#axes.plot(x_j, y_j, '.', color='black', label=r'$(x,y)_{J}$')
-
-
-
-
-#t = np.linspace(0,len(x_s) , len(x_s))
-axes[0].plot(t, x_s, '.', color='yellow', label=r'$(x)_{S}$')
-axes[1].plot(t, y_s, '.', color='yellow', label=r'$(y)_{S}$')
-axes[2].plot(t, x_j, '.', color='black', label=r'$(x)_{J}$')
-axes[3].plot(t, y_j, '.', color='black', label=r'$(y)_{J}$')
-axes[4].plot(t, x_t, '.', color='blue', label=r'$(x)_{T}$')
-axes[5].plot(t, y_t, '.', color='blue', label=r'$(y)_{T}$')
-
-
-
-
-for i in range(0,6):
-    axes[i].legend()
-
-
-
-# Se ajustan demás detalles del gráfico.
-
-# axes.set_xlabel('x', fontsize=12)
-# axes.set_ylabel('y', fontsize=12)
-# axes.legend(loc='upper left')
-# axes.grid(True, linestyle='--')
-# axes.set_title("x vs y", fontsize=14)
-plt.tight_layout()
-fig.savefig('plot.pdf')
+    axes[2].plot(x_t, y_t, ".", color="cyan", label=r"$(x',y')_{T}$")
+    axes[2].set_xlabel("x'", fontsize=12)
+    axes[2].set_ylabel("y'", fontsize=12)
+    axes[2].legend(loc='upper right')
+    axes[2].grid(True, linestyle='--')
+    axes[2].set_title("y' vs. x'", fontsize=14)
+    axes[2].ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+    plt.tight_layout()
+    pdf.savefig()  # saves the current figure into a pdf page
+    plt.close()
