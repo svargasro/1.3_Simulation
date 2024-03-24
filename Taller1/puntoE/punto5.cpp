bd25384a0ff9ba8a57b1b5c4c54ef194174ce73e
svargasro <svargasro@unal.edu.co>
2024-03-23 23:18:22 -0500

    TerminandoE

--- Taller1/puntoE/punto5.cpp
+++ Taller1/puntoE/punto5.cpp
@@ -24,24 +24,24 @@ const double Um2chiplusxi=1-2*(chi+xi);
 
 //--------------- Declarar las clases-----------
 class Cuerpo;
-class Colisionador;
+class Interaction;
 
 //--------- Declarar las interfases de las clases---------
 class Cuerpo{
 private:
-  vector3D r,V,F; double m,R;
+  vector3D r,V,F; double m;
 public:
-  void Inicie(double x0,double y0,double z0,double Vx0,double Vy0,double Vz0,double m0,double R0);
+  void Inicie(double x0,double y0,double z0,double Vx0,double Vy0,double Vz0,double m0);
   void BorreFuerza(void){F.load(0,0,0);}; // Inline
   void SumeFuerza(vector3D dF){F+=dF;}; // Inline
   void Mueva_r(double dt,double coeficiente);
   void Mueva_V(double dt,double coeficiente);
   double Getx(void){return r.x();}; // Inline
   double Gety(void){return r.y();}; // Inline
-  friend class Colisionador;
+  friend class Interaction;
 };
 
-class Colisionador{
+class Interaction{
 private:
 public:
   void CalculeTodasLasFuerzas(Cuerpo * Planetas);
@@ -53,8 +53,8 @@ public:
 
 //------- Funciones de la clase cuerpo --------
 void Cuerpo::Inicie(double x0,double y0,double z0,
-                    double Vx0,double Vy0,double Vz0,double m0,double R0){
-  r.load(x0,y0,z0);  V.load(Vx0,Vy0,Vz0); m=m0; R=R0;
+                    double Vx0,double Vy0,double Vz0,double m0){
+  r.load(x0,y0,z0);  V.load(Vx0,Vy0,Vz0); m=m0;
 }
 
 void Cuerpo::Mueva_r(double dt,double coeficiente){
@@ -66,8 +66,8 @@ void Cuerpo::Mueva_V(double dt,double coeficiente){
 }
 
 
-//------- Funciones de la clase Colisionador --------
-void Colisionador::CalculeTodasLasFuerzas(Cuerpo * Planeta){
+//------- Funciones de la clase Interaction --------
+void Interaction::CalculeTodasLasFuerzas(Cuerpo * Planeta){
   int i,j;
   //Borro las fuerzas de todos los planetas
   for(i=0;i<N;i++)
@@ -77,7 +77,7 @@ void Colisionador::CalculeTodasLasFuerzas(Cuerpo * Planeta){
     for(j=0;j<i;j++)
       CalculeFuerzaEntre(Planeta[i],Planeta[j]);
 }
-void Colisionador::CalculeFuerzaEntre(Cuerpo & Planeta1,Cuerpo & Planeta2){
+void Interaction::CalculeFuerzaEntre(Cuerpo & Planeta1,Cuerpo & Planeta2){
   double m1=Planeta1.m, m2=Planeta2.m;
   vector3D r21=Planeta2.r-Planeta1.r;
   double r2=r21.norm2();
@@ -90,94 +90,85 @@ void Colisionador::CalculeFuerzaEntre(Cuerpo & Planeta1,Cuerpo & Planeta2){
 int main(){
   double r=1000.0; //Distancia entre Júpiter y el Sol.
   double m0= 1047.0;  //Masa del sol.
-  double m1= 1; //Masa de Júpiter.
-  double M=m0+m1, mu=m0*m1/M;
-  double x0=-m1*r/M,x1=m0*r/M;
-  double omega=sqrt(G*M/(r*r*r)); double T=2*M_PI/omega;
-  cout<<"Perido: "<<T<<endl;
-  double V0=omega*x0, V1=omega*x1;
+  double m1=1; //Masa de Júpiter.
+  double M=m0+m1;
+  double x0=-m1*r/M,x1=m0*r/M; //Condición inicial en la posición para movimiento circular.
+  double omega=sqrt(G*M/(r*r*r)); //Condición inicial en la velocidad angular para movimiento circular.
+  double T=2*M_PI/omega;
+  double V0=omega*x0, V1=omega*x1; //Condiciones iniciales en la velocidad para movimiento circular.
+
   //Variables tercer cuerpo.
   double x2 = x1*cos(M_PI/3.0); //Posición inicial en x
-  // x2= x2 + 3*x2/1000.0; //Perturbación en posición
-
   double y2 = x1*sin(M_PI/3.0); //Posición inicial en y
-  //y2= y2 + 5*y2/1000.0; //Perturbación en posición
-
   double V2x = -omega*y2; //Velocidad inicial en x
 
-  V2x = V2x + 5*V2x/1000.0;
+  V2x = V2x + 5*V2x/1000.0; //Perturbación de 5 partes por mil en la velocidad en x.
 
   double V2y = omega*x2; //Velocidad inicial en y
 
+  double m2= 0.005; //Masa del tercer cuerpo
 
+  int numOrbitas = 20; //Número total de órbitas.
 
-  double m2= 0.005;
-
-  int numOrbitas = 20;
   double t, ttotal=numOrbitas*T;
 
   double dt=1; //Paso de tiempo
   Cuerpo Planeta[N];
-  Colisionador Newton;
+  Interaction Newton;
   int i;
 
   //INICIO
-  //---------------(x0,y0,z0,Vx0,   Vy0,Vz0,m0,R0)
-  Planeta[0].Inicie(x0, 0, 0,  0, V0,  0,m0,1.0);
-  Planeta[1].Inicie(x1, 0, 0,  0, V1,  0,m1,0.5);
-  Planeta[2].Inicie(x2, y2, 0,  V2x, V2y,  0,m2,0.5);
-
-  cout<<"Inicial sol"<<x0<<endl;
-  cout<<"Inicial Júpiter"<<x1<<endl;
-  cout<<"Inicial troyano_x"<<x2<<endl;
-  cout<<"Inicial troyano_y"<<y2<<endl;
-
-
-
+  //---------------(x0,y0,z0,Vx0,   Vy0,Vz0,m0)
+  Planeta[0].Inicie(x0, 0, 0,  0, V0,  0,m0);
+  Planeta[1].Inicie(x1, 0, 0,  0, V1,  0,m1);
+  Planeta[2].Inicie(x2, y2, 0,  V2x, V2y,  0,m2);
 
   std::ofstream fout;
   fout.open("data.txt");
-  fout.precision(15);
-  fout.setf(std::ios::scientific);
 
   //Se calcula el movimiento.
   for(t=0;t<ttotal;t+=dt){
 
-  double x_s = Planeta[0].Getx();
-  double y_s = Planeta[0].Gety();
-  double x_j = Planeta[1].Getx();
-  double y_j = Planeta[1].Gety();
+    //Se declarar variables para las coordenadas del Sol y Júpiter en el sistema sin rotar puesto que serán utilizadas varias veces.
+    double x_s = Planeta[0].Getx();
+    double y_s = Planeta[0].Gety();
+    double x_j = Planeta[1].Getx();
+    double y_j = Planeta[1].Gety();
 
-  double x_t = Planeta[2].Getx();
-  double y_t = Planeta[2].Gety();
+    double x_t = Planeta[2].Getx();
+    double y_t = Planeta[2].Gety();
 
 
-  double omegaT= omega*t;
-  double x_sp = x_s*cos(omegaT) + y_s*sin(omegaT);
-  double y_sp = -x_s*sin(omegaT) + y_s*cos(omegaT);
+    double omegaT= omega*t; //Ángulo que se recorre por unidad de tiempo.
 
-  double x_jp = x_j*cos(omegaT) + y_j*sin(omegaT);
-  double y_jp = -x_j*sin(omegaT) + y_j*cos(omegaT);
+    /*Se cambia al sistema rotante (primado), que satisface el cambio de base dado por la matriz de rotación con theta, el ángulo de giro,
+      igual a omega*T. */
 
-  double x_tp = x_t*cos(omegaT) + y_t*sin(omegaT);
-  double y_tp = -x_t*sin(omegaT) + y_t*cos(omegaT);
+    double x_sp = x_s*cos(omegaT) + y_s*sin(omegaT);
+    double y_sp = -x_s*sin(omegaT) + y_s*cos(omegaT);
 
-  // fout<<x_sp<<" "<<y_sp<<" "<<x_jp<<" "<<y_jp<<endl;
-  fout<<x_sp<<" "<<y_sp<<" "<<x_jp<<" "<<y_jp<<" "<<" "<<x_tp<<" "<<y_tp<<" "<<t<<endl;
-  //   fout<<Planeta[0].Getx()<<" "<<Planeta[0].Gety()<<" "<<Planeta[1].Getx()<<" "<<Planeta[1].Gety()<<endl;
+    double x_jp = x_j*cos(omegaT) + y_j*sin(omegaT);
+    double y_jp = -x_j*sin(omegaT) + y_j*cos(omegaT);
 
-  for(i=0;i<N;i++) Planeta[i].Mueva_r(dt,xi);
-  Newton.CalculeTodasLasFuerzas(Planeta); for(i=0;i<N;i++) Planeta[i].Mueva_V(dt,Um2lambdau2);
-  for(i=0;i<N;i++) Planeta[i].Mueva_r(dt,chi);
-  Newton.CalculeTodasLasFuerzas(Planeta); for(i=0;i<N;i++) Planeta[i].Mueva_V(dt,lambda);
-  for(i=0;i<N;i++) Planeta[i].Mueva_r(dt,Um2chiplusxi);
-  Newton.CalculeTodasLasFuerzas(Planeta); for(i=0;i<N;i++)Planeta[i].Mueva_V(dt,lambda);
-  for(i=0;i<N;i++) Planeta[i].Mueva_r(dt,chi);
-  Newton.CalculeTodasLasFuerzas(Planeta); for(i=0;i<N;i++)Planeta[i].Mueva_V(dt,Um2lambdau2);
-  for(i=0;i<N;i++) Planeta[i].Mueva_r(dt,xi);
+    //Se agrega el cálculo de las coordenadas del planeta troyano en el sistema primado.
+    double x_tp = x_t*cos(omegaT) + y_t*sin(omegaT);
+    double y_tp = -x_t*sin(omegaT) + y_t*cos(omegaT);
+
+
+    fout<<x_sp<<" "<<y_sp<<" "<<x_jp<<" "<<y_jp<<" "<<" "<<x_tp<<" "<<y_tp<<" "<<t<<endl; //Se pasan las coordenadas del sistema primado del Sol, Júpiter y del planeta troyano, además del tiempo.
+
+    for(i=0;i<N;i++) Planeta[i].Mueva_r(dt,xi);
+    Newton.CalculeTodasLasFuerzas(Planeta); for(i=0;i<N;i++) Planeta[i].Mueva_V(dt,Um2lambdau2);
+    for(i=0;i<N;i++) Planeta[i].Mueva_r(dt,chi);
+    Newton.CalculeTodasLasFuerzas(Planeta); for(i=0;i<N;i++) Planeta[i].Mueva_V(dt,lambda);
+    for(i=0;i<N;i++) Planeta[i].Mueva_r(dt,Um2chiplusxi);
+    Newton.CalculeTodasLasFuerzas(Planeta); for(i=0;i<N;i++)Planeta[i].Mueva_V(dt,lambda);
+    for(i=0;i<N;i++) Planeta[i].Mueva_r(dt,chi);
+    Newton.CalculeTodasLasFuerzas(Planeta); for(i=0;i<N;i++)Planeta[i].Mueva_V(dt,Um2lambdau2);
+    for(i=0;i<N;i++) Planeta[i].Mueva_r(dt,xi);
 
   }
 
   fout.close();
-return 0;
+  return 0;
 }
